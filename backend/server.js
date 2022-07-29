5e35af8fc1dd3caefcf67157bee19ef501ee1f61
Ryan Jacobs <Ryan.jacobs.739@my.csun.edu>
2022-07-28 21:24:18 -0700

    Created backend with express/mongoose, created db in MongoDB.  Connected backend to db

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
+const dbConfig = require('./database')
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Node Server Started using nodemon`));
